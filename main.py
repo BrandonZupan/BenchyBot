8d17220fc2e7746e69f594c4a2d48bd6394af415
Brandon Zupan <zupan.brandon@utexas.edu>
2019-06-07 02:06:14 +0000

    Scans through dict and sends

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import discord
 
 #Load the commands from the file images.txt
+with open("images.txt") as fin:
+     rows = ( line.split('`') for line in fin )
+     images = { row[0]:row[1:] for row in rows }
 
-client = discord.client()
+#The commands are now in a dictionary with each entry pointing to the message, in an array of one entry
+
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -21,13 +27,27 @@ async def on_message(message):
         print(command)
 
         #Hello: Replies hello
-        if command[0] == "$hello":
+        if command[0] == "!hello":
             name = "  "
             name = message.author
             name = str(name)
             #print(name)
             #name = name.split("#")
             await message.channel.send('Hello ' + name.split('#')[0] + '!')
+
+        #Print all the possible image commands
+        if command[0] == "!listcommands":
+            if message.author.guild_permissions.administrator:
+                possiblecommands = ""
+                for command in images:
+                    possiblecommands += command + ', '
+                await message.channel.send(possiblecommands)
+
+        #Look if its in the images file
+        elif command[0] in images.keys():
+            await message.channel.send(images[command[0]][0])
+
+        
 
 
 
