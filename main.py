95d5be3730d5eb317dfa964b44b737bc52e1f9f3
Brandon Zupan <zupan.brandon@utexas.edu>
2019-07-21 12:12:34 -0500

    Made it complient

--- main.py
+++ main.py
@@ -12,49 +12,58 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from joinGraph import joinChartGenerator, userCSVgenerator
 
+
 #Start logging
 logging.basicConfig(level=logging.INFO)
 
 #SQL Database
-engine = create_engine('sqlite:///responces.db', echo=False)
-Base = declarative_base()
+ENGINE = create_engine('sqlite:///responces.db', echo=False)
+BASE = declarative_base()
 
-class ccCommand(Base):
+class CCCommand(BASE):
+    """
+    Makes object that database undserstands
+    """
     __tablename__ = "imageCommands"
-    
+
     #Has a column for the ID, name, and responce
     #id = Column(Integer, primary_key=True)
     name = Column(String, primary_key=True)
     responce = Column(String)
 
-#Create the Table   
-Base.metadata.create_all(engine)
+#Create the Table
+BASE.metadata.create_all(ENGINE)
 
 #Start the SQL session
-Session = sessionmaker(bind=engine)
-session = Session()
+SESSION = sessionmaker(bind=ENGINE)
+COMMANDDB = SESSION()
 
 #Discord client
-client = commands.Bot(command_prefix='!')
+CLIENT = commands.Bot(command_prefix='!')
 
-@client.event
+@CLIENT.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    """
+    Runs when bot connects
+    """
+    print('We have logged in as {0.user}'.format(CLIENT))
 
-#Used to check if user is admin
 async def is_admin(ctx):
+    """
+    Checks if user is an admon
+    """
     return ctx.message.author.guild_permissions.administrator
 
-async def in_secretChannel(ctx):
+async def in_secret_channel(ctx):
     """Checks if a command was used in a secret channel"""
-    secretChannels = {
+    secret_channels = {
         'command-sandbox': 339978089411117076,
         'srsbusiness': 525494034203017246,
         'lets-kill-this-bot': 532781500471443477
     }
-    usedChannel = ctx.channel.id
-    for channel in secretChannels:
-        if secretChannels[channel] == usedChannel:
+    used_channel = ctx.channel.id
+    for channel in secret_channels:
+        if secret_channels[channel] == used_channel:
             return True
 
     #It dont exist
@@ -64,7 +73,7 @@ async def in_secretChannel(ctx):
 ###Commands###
 ##############
 
-@client.command(name='hello')
+@CLIENT.command(name='hello')
 async def hello(ctx):
     """
     Says hello to the user
@@ -72,21 +81,27 @@ async def hello(ctx):
     await ctx.send("Hello " + str(ctx.author).split('#')[0] + '!')
 
 
-@client.command(name='usergraph', hidden=True)
+@CLIENT.command(name='usergraph', hidden=True)
 @commands.check(is_admin)
-async def usergraph(ctx):
+async def user_graph(ctx):
+    """
+    Generates a graph of when users joined
+    """
     await joinChartGenerator(ctx)
 
-@client.command(name='userlist', hidden=True)
+@CLIENT.command(name='userlist', hidden=True)
 @commands.check(is_admin)
-@commands.check(in_secretChannel)
-async def userlist(ctx):
+@commands.check(in_secret_channel)
+async def user_list(ctx):
+    """
+    Generates a CSV of when all members joined
+    """
     await userCSVgenerator(ctx)
 
-@client.command(name='cc', hidden=True)
+@CLIENT.command(name='cc', hidden=True)
 @commands.check(is_admin)
-@commands.check(in_secretChannel)
-async def cc(ctx, *args):
+@commands.check(in_secret_channel)
+async def cc_command(ctx, *args):
     """
     Modifies the command database
 
@@ -97,67 +112,72 @@ async def cc(ctx, *args):
     Bot will confirm with :ok_hand:
     """
     #If zero arguments, list all commands
-    if len(args) == 0:
-        commandList = str()
-        for instance in session.query(ccCommand).order_by(ccCommand.name):
-            commandList += instance.name + ' '
+    if not args:
+        command_list = str()
+        for instance in COMMANDDB.query(CCCommand).order_by(CCCommand.name):
+            command_list += instance.name + ' '
         embed = discord.Embed(
-            title = "Command: !cc", 
-            description = commandList,
-            color = 0xBF5700)
+            title="Command: !cc",
+            description=command_list,
+            color=0xBF5700)
         await ctx.send(embed=embed)
 
     #If one argument, delete that command
     if len(args) == 1:
         #print(args[0])
-        victim = session.query(ccCommand).filter_by(name=args[0]).one()
+        victim = COMMANDDB.query(CCCommand).filter_by(name=args[0]).one()
         #print(victim.responce)
-        session.delete(victim)
-        session.commit()
+        COMMANDDB.delete(victim)
+        COMMANDDB.commit()
         await ctx.message.add_reaction('👌')
         logging.info(ctx.author.name + " deleted " + victim.name)
 
     #If 2 or more arguments, combine them and modify database
     if len(args) >= 2:
-        #newCC = ccCommand(args[0], ' '.join(args[1:]))
+        #newCC = CCCommand(args[0], ' '.join(args[1:]))
         #await ctx.send("Command " + newCC.name + " with link " + newCC.responce)
-        newCC = ccCommand(name=args[0].lower(), responce=' '.join(args[1:]))
-        session.merge(newCC)
-        session.commit()
+        new_cc = CCCommand(name=args[0].lower(), responce=' '.join(args[1:]))
+        COMMANDDB.merge(new_cc)
+        COMMANDDB.commit()
         #await ctx.send("Command " + newCC.name + " with link " + newCC.responce)
         await ctx.message.add_reaction('👌')
-        logging.info(ctx.author.name + " added " + newCC.name + " with responce " + newCC.responce)
+        logging.info("%s added %s with responce %s", ctx.author.name, new_cc.name, new_cc.responce)
 
-@client.command(name='listcommands', hidden=True)
+@CLIENT.command(name='listcommands', hidden=True)
 @commands.check(is_admin)
-@commands.check(in_secretChannel)
-async def listcommands(ctx):
-    commandList = str()
-    for instance in session.query(ccCommand).order_by(ccCommand.name):
-        commandList += instance.name + ' '
+@commands.check(in_secret_channel)
+async def list_commands(ctx):
+    """
+    Lists all commands in the database
+    """
+    command_list = str()
+    for instance in COMMANDDB.query(CCCommand).order_by(CCCommand.name):
+        command_list += instance.name + ' '
     embed = discord.Embed(
-        title = "Command: !cc", 
-        description = commandList,
-        color = 0xBF5700)
+        title="Command: !cc",
+        description=command_list,
+        color=0xBF5700)
     await ctx.send(embed=embed)
 
-@client.event
+@CLIENT.event
 async def on_command_error(ctx, error):
+    """
+    Parses command database since library sees them as an error
+    """
     if isinstance(error, commands.errors.CommandNotFound):
         #await ctx.send(ctx.message.content)
         command = ctx.message.content.lower()
         command = command.split(" ", 1)
 
         #Look if its in the database
-        for instance in session.query(ccCommand).order_by(ccCommand.name):
+        for instance in COMMANDDB.query(CCCommand).order_by(CCCommand.name):
             if instance.name == command[0][1:]:
                 await ctx.send(instance.responce)
                 return
     else:
         print(error)
-  
 
 
 with open('key.txt', 'r') as keyFile:
-    key = keyFile.read()
-client.run(key)
+    KEY = keyFile.read()
+CLIENT.run(KEY)
